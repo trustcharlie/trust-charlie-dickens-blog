d217c427524a47e460a279e8f7bea1926e0b7753
samuel norbury <samuel@norbury.nl>
2021-01-19 20:43:02 +0100

    some defaults applied

---
+++ content/blog/2021-01-19-first-post/index.md
@@ -0,0 +1,11 @@
+---
+path: "/hello-world"
+date: 2017-07-12T17:12:33.962Z
+title: "My First Gatsby Post"
+description: "First test gatsby post"
+---
+
+
+ooooh-weeee, my first blog post!
+
+![](bla.jpg)
--- src/components/bio.js
+++ src/components/bio.js
@@ -27,15 +27,12 @@ const Bio = () => {
             name
             summary
           }
-          social {
-            twitter
-          }
         }
       }
     }
   `)
 
-  const { author, social } = data.site.siteMetadata
+  const { author} = data.site.siteMetadata
   return (
     <div
       style={{
@@ -57,11 +54,8 @@ const Bio = () => {
         }}
       />
       <p>
-        Written by <strong>{author.name}</strong> {author.summary}
+        Written by <strong>{author.name}</strong>, {author.summary}
         {` `}
-        <a href={`https://twitter.com/${social.twitter}`}>
-          You should follow him on Twitter
-        </a>
       </p>
     </div>
   )
